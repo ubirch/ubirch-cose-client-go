0d692dde91b58a92abb48339b807a9ff3cb390b6
Roxana Meixner <roxana.meixner@ubirch.com>
2021-04-23 10:46:27 +0200

    use actual CBOR encoded from JSON as payload

--- cose_signer_test.go
+++ cose_signer_test.go
@@ -17,6 +17,8 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"github.com/google/uuid"
 	"github.com/ubirch/ubirch-protocol-go/ubirch/v2"
 	"testing"
@@ -26,7 +28,7 @@ var (
 	uid    = uuid.MustParse("d1b7eb09-d1d8-4c63-b6a5-1c861a6477fa")
 	key, _ = base64.StdEncoding.DecodeString("YUm0Xy475i7gnGNSnNJUriHQm33Uf+b/XHqZwjFluwM=")
 
-	payload = []byte("payload bytes")
+	payloadJSON = "{\"test\": 123}"
 )
 
 func TestCoseSign(t *testing.T) {
@@ -37,14 +39,19 @@ func TestCoseSign(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	toBeSigned, err := coseSigner.GetSigStructBytes(payload)
+	payloadCBOR, err := getCBORFromJSON(coseSigner, []byte(payloadJSON))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	toBeSigned, err := coseSigner.GetSigStructBytes(payloadCBOR)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	hash := sha256.Sum256(toBeSigned)
 
-	coseBytes, err := coseSigner.getSignedCOSE(uid, hash, payload)
+	coseBytes, err := coseSigner.getSignedCOSE(uid, hash, payloadCBOR)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -66,4 +73,15 @@ func setupCrypto(t *testing.T) *ubirch.ECDSACryptoContext {
 	t.Logf("public key: %x", pubKey)
 
 	return cryptoCtx
+}
+
+func getCBORFromJSON(encoder *CoseSigner, jsonData []byte) ([]byte, error) {
+	var reqDump interface{}
+
+	err := json.Unmarshal(jsonData, &reqDump)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse JSON request body: %v", err)
+	}
+
+	return encoder.encMode.Marshal(reqDump)
 }
