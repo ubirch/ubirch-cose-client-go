f889f649b7207c5df98accdbb82d253b962041ae
Roxana Meixner <roxana.meixner@ubirch.com>
2021-05-21 19:33:54 +0200

    LegacyFileManager

--- main/migrator.go
+++ main/migrator.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func Migrate(c *Config) error {
+func MigrateFileToDB(c *Config) error {
 	identities := new([]*Identity)
 
 	err := c.loadIdentitiesFile(identities)
@@ -20,12 +21,7 @@ func Migrate(c *Config) error {
 		return err
 	}
 
-	fileManager, err := NewFileManager(c.configDir)
-	if err != nil {
-		return err
-	}
-
-	err = getKeysFromLegacyCtx(fileManager, identities)
+	err = getKeysFromFile(c.configDir, identities)
 	if err != nil {
 		return err
 	}
@@ -44,14 +40,19 @@ func Migrate(c *Config) error {
 	return nil
 }
 
-func getKeysFromLegacyCtx(f *FileManager, identities *[]*Identity) (err error) {
+func getKeysFromFile(configDir string, identities *[]*Identity) (err error) {
+	fileManager, err := NewFileManager(configDir)
+	if err != nil {
+		return err
+	}
+
 	for _, i := range *identities {
-		i.PrivateKey, err = f.GetPrivateKey(i.Uid)
+		i.PrivateKey, err = fileManager.GetPrivateKey(i.Uid)
 		if err != nil {
 			return fmt.Errorf("%s: %v", i.Uid, err)
 		}
 
-		i.PublicKey, err = f.GetPublicKey(i.Uid)
+		i.PublicKey, err = fileManager.GetPublicKey(i.Uid)
 		if err != nil {
 			return fmt.Errorf("%s: %v", i.Uid, err)
 		}
@@ -97,4 +98,106 @@ func migrateIdentities(dm *DatabaseManager, identities *[]*Identity) error {
 	}
 
 	return dm.CloseTransaction(tx, Commit)
+}
+
+func MigrateLegacyFileToDB(c *Config) error {
+	identities := new([]*Identity)
+
+	err := c.loadIdentitiesFile(identities)
+	if err != nil {
+		return err
+	}
+
+	err = getKeysFromFileWithDecryption(c, identities)
+	if err != nil {
+		return err
+	}
+
+	dbManager, err := NewSqlDatabaseInfo(c)
+	if err != nil {
+		return err
+	}
+
+	p, err := NewProtocol(dbManager, c.secretBytes, nil)
+	if err != nil {
+		return err
+	}
+
+	err = migrateIdentitiesWithEncryption(p, identities)
+	if err != nil {
+		return err
+	}
+
+	log.Infof("successfully migrated file based context into database")
+	return nil
+}
+
+func getKeysFromFileWithDecryption(c *Config, identities *[]*Identity) (err error) {
+	log.Infof("getting existing identities from file system")
+
+	secret16Bytes, err := base64.StdEncoding.DecodeString(c.Secret16Base64)
+	if err != nil {
+		return fmt.Errorf("unable to decode base64 encoded secret for legacy key store decoding (%s): %v", c.Secret16Base64, err)
+	}
+	if len(secret16Bytes) != 16 {
+		return fmt.Errorf("invalid secret for legacy key store decoding: secret length must be 16 bytes (is %d)", len(secret16Bytes))
+	}
+
+	fileManager, err := NewLegacyFileManager(c.configDir, secret16Bytes)
+	if err != nil {
+		return err
+	}
+
+	for _, i := range *identities {
+		i.PrivateKey, err = fileManager.GetPrivateKey(i.Uid)
+		if err != nil {
+			return fmt.Errorf("%s: %v", i.Uid, err)
+		}
+
+		i.PublicKey, err = fileManager.GetPublicKey(i.Uid)
+		if err != nil {
+			return fmt.Errorf("%s: %v", i.Uid, err)
+		}
+	}
+
+	return nil
+}
+
+func migrateIdentitiesWithEncryption(p *Protocol, identities *[]*Identity) error {
+	log.Infof("starting migration...")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	tx, err := p.StartTransaction(ctx)
+	if err != nil {
+		return err
+	}
+
+	for i, id := range *identities {
+		log.Infof("%4d: %s", i+1, id.Uid)
+
+		if len(id.PrivateKey) == 0 {
+			return fmt.Errorf("%s: empty private key", id.Uid)
+		}
+
+		if len(id.PublicKey) == 0 {
+			return fmt.Errorf("%s: empty public key", id.Uid)
+		}
+
+		if len(id.AuthToken) == 0 {
+			return fmt.Errorf("%s: empty auth token", id.Uid)
+		}
+
+		err = p.StoreNewIdentity(tx, *id)
+		if err != nil {
+			if err == ErrExists {
+				log.Warnf("%s: %v -> skip", id.Uid, err)
+			} else {
+				return err
+			}
+		}
+	}
+
+	return p.CloseTransaction(tx, Commit)
 }
