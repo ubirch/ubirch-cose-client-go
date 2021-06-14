966d8acca0cdb93baac1f290b1039d131ac7cb66
Roxana Meixner <roxana.meixner@ubirch.com>
2021-06-14 10:29:46 +0200

    db load test and further enhancements

--- main/database_test.go
+++ main/database_test.go
@@ -4,7 +4,11 @@ import (
 	"bytes"
 	"context"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"math/rand"
+	"os"
+	"sync"
 	"testing"
 
 	"github.com/google/uuid"
@@ -12,155 +16,282 @@ import (
 
 const (
 	TestTableName = "test_cose_identity"
-	TestUUID      = "2336c75d-a14a-47dd-80d9-3cbe9d560433"
-	TestAuthToken = "TEST_auth"
-	TestPrivKey   = "Qkp+ZVAlEKCQNvI+OCbY7LKcQVW5iKfFMfzedTI3uG0="
-	TestPubKey    = "bvXP3mQ42hXpcqo0ms7Lr1n6Q4L5CsS8HXk0mdXlsXLwYjd35jLlX3iHrXMgUH92N8ujbZ3h3TnLk8a0GikUbg=="
-)
-
-var (
-	testIdentity = initTestIdentity()
 )
 
 func TestDatabaseManager(t *testing.T) {
-	dbManager, err := initDB()
+	dm, err := initDB()
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer cleanUp(t, dbManager)
+	defer cleanUp(t, dm)
+
+	testIdentity := generateRandomIdentity()
 
 	// check not exists
-	exists, err := dbManager.ExistsPublicKey(testIdentity.Uid)
+	exists, err := dm.ExistsPublicKey(testIdentity.Uid)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	if exists {
-		t.Errorf("dbManager.ExistsPublicKey returned TRUE")
+		t.Error("ExistsPublicKey returned TRUE")
 	}
 
-	exists, err = dbManager.ExistsPrivateKey(testIdentity.Uid)
+	exists, err = dm.ExistsPrivateKey(testIdentity.Uid)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	if exists {
-		t.Errorf("dbManager.ExistsPrivateKey returned TRUE")
+		t.Error("ExistsPrivateKey returned TRUE")
 	}
 
-	exists, err = dbManager.ExistsUuidForPublicKey(testIdentity.PublicKey)
+	exists, err = dm.ExistsUuidForPublicKey(testIdentity.PublicKey)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	if exists {
-		t.Error("dbManager.ExistsUuidForPublicKey returned TRUE")
+		t.Error("ExistsUuidForPublicKey returned TRUE")
 	}
 
 	// store identity
-	tx, err := dbManager.StartTransaction(context.Background())
+	tx, err := dm.StartTransaction(context.Background())
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	err = dbManager.StoreNewIdentity(tx, *testIdentity)
+	err = dm.StoreNewIdentity(tx, *testIdentity)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	err = dbManager.CloseTransaction(tx, Commit)
+	err = dm.CloseTransaction(tx, Commit)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// check exists
-	exists, err = dbManager.ExistsPublicKey(testIdentity.Uid)
+	exists, err = dm.ExistsPublicKey(testIdentity.Uid)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	if !exists {
-		t.Errorf("dbManager.ExistsPublicKey returned FALSE")
+		t.Error("ExistsPublicKey returned FALSE")
 	}
 
-	exists, err = dbManager.ExistsPrivateKey(testIdentity.Uid)
+	exists, err = dm.ExistsPrivateKey(testIdentity.Uid)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	if !exists {
-		t.Errorf("dbManager.ExistsPrivateKey returned FALSE")
+		t.Error("ExistsPrivateKey returned FALSE")
 	}
 
-	exists, err = dbManager.ExistsUuidForPublicKey(testIdentity.PublicKey)
+	exists, err = dm.ExistsUuidForPublicKey(testIdentity.PublicKey)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	if !exists {
-		t.Error("public key not found")
+		t.Error("ExistsUuidForPublicKey returned FALSE")
 	}
 
 	// get attributes
-	auth, err := dbManager.GetAuthToken(testIdentity.Uid)
+	auth, err := dm.GetAuthToken(testIdentity.Uid)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	if auth != testIdentity.AuthToken {
 		t.Error("GetAuthToken returned unexpected value")
 	}
 
-	priv, err := dbManager.GetPrivateKey(testIdentity.Uid)
+	priv, err := dm.GetPrivateKey(testIdentity.Uid)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	if !bytes.Equal(priv, testIdentity.PrivateKey) {
 		t.Error("GetPrivateKey returned unexpected value")
 	}
 
-	pub, err := dbManager.GetPublicKey(testIdentity.Uid)
+	pub, err := dm.GetPublicKey(testIdentity.Uid)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	if !bytes.Equal(pub, testIdentity.PublicKey) {
 		t.Error("GetPublicKey returned unexpected value")
 	}
 
-	uid, err := dbManager.GetUuidForPublicKey(testIdentity.PublicKey)
+	uid, err := dm.GetUuidForPublicKey(testIdentity.PublicKey)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	if !bytes.Equal(uid[:], testIdentity.Uid[:]) {
 		t.Error("GetUuidForPublicKey returned unexpected value")
 	}
 }
 
-func initDB() (*DatabaseManager, error) {
-	conf := &Config{}
-	err := conf.Load("", "config.json")
+func TestDatabaseLoad(t *testing.T) {
+	wg := &sync.WaitGroup{}
+
+	dm, err := initDB()
 	if err != nil {
-		return nil, fmt.Errorf("ERROR: unable to load configuration: %s", err)
+		t.Fatal(err)
+	}
+	defer cleanUp(t, dm)
+
+	// generate identities
+	var testIdentities []*Identity
+	for i := 0; i < 10000; i++ {
+		testIdentities = append(testIdentities, generateRandomIdentity())
 	}
 
-	return NewSqlDatabaseInfo(conf.PostgresDSN, TestTableName)
+	// store identities
+	for i, testId := range testIdentities {
+		go func(idx int, identity *Identity) {
+			err := storeIdentity(dm, identity, wg)
+			if err != nil {
+				t.Errorf("%s: identity could not be stored %v", identity.Uid, err)
+			}
+		}(i, testId)
+	}
+	wg.Wait()
+
+	// check identities
+	for _, testId := range testIdentities {
+		go func(id *Identity) {
+			err := checkIdentity(dm, id, wg)
+			if err != nil {
+				t.Errorf("%s: %v", id.Uid, err)
+			}
+		}(testId)
+	}
+	wg.Wait()
 }
 
-func initTestIdentity() *Identity {
-	priv, _ := base64.StdEncoding.DecodeString(TestPrivKey)
-	pub, _ := base64.StdEncoding.DecodeString(TestPubKey)
+type dbConfig struct {
+	PostgresDSN string
+}
+
+func initDB() (*DatabaseManager, error) {
+	fileHandle, err := os.Open("config.json")
+	if err != nil {
+		return nil, err
+	}
+	defer fileHandle.Close()
+
+	c := &dbConfig{}
+	err = json.NewDecoder(fileHandle).Decode(c)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewSqlDatabaseInfo(c.PostgresDSN, TestTableName)
+}
+
+func cleanUp(t *testing.T, dm *DatabaseManager) {
+	dropTableQuery := fmt.Sprintf("DROP TABLE %s;", TestTableName)
+	_, err := dm.db.Exec(dropTableQuery)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func generateRandomIdentity() *Identity {
+	priv := make([]byte, 32)
+	rand.Read(priv)
+
+	pub := make([]byte, 64)
+	rand.Read(pub)
+
+	auth := make([]byte, 16)
+	rand.Read(auth)
 
 	return &Identity{
-		Uid:        uuid.MustParse(TestUUID),
+		Uid:        uuid.New(),
 		PrivateKey: priv,
 		PublicKey:  pub,
-		AuthToken:  TestAuthToken,
+		AuthToken:  base64.StdEncoding.EncodeToString(auth),
 	}
 }
 
-func cleanUp(t *testing.T, dbManager *DatabaseManager) {
-	deleteQuery := fmt.Sprintf("DELETE FROM %s WHERE uid = $1;", TestTableName)
-	_, err := dbManager.db.Exec(deleteQuery, TestUUID)
+func storeIdentity(dm *DatabaseManager, id *Identity, wg *sync.WaitGroup) error {
+	wg.Add(1)
+	defer wg.Done()
+
+	tx, err := dm.StartTransaction(context.Background())
 	if err != nil {
-		t.Error(err)
+		return err
 	}
 
-	dropTableQuery := fmt.Sprintf("DROP TABLE %s;", TestTableName)
-	_, err = dbManager.db.Exec(dropTableQuery)
+	err = dm.StoreNewIdentity(tx, *id)
 	if err != nil {
-		t.Error(err)
+		return err
 	}
+
+	err = dm.CloseTransaction(tx, Commit)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func checkIdentity(dm *DatabaseManager, id *Identity, wg *sync.WaitGroup) error {
+	wg.Add(1)
+	defer wg.Done()
+
+	exists, err := dm.ExistsPublicKey(id.Uid)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return fmt.Errorf("ExistsPublicKey returned FALSE")
+	}
+
+	exists, err = dm.ExistsPrivateKey(id.Uid)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return fmt.Errorf("ExistsPrivateKey returned FALSE")
+	}
+
+	exists, err = dm.ExistsUuidForPublicKey(id.PublicKey)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return fmt.Errorf("ExistsUuidForPublicKey returned FALSE")
+	}
+
+	auth, err := dm.GetAuthToken(id.Uid)
+	if err != nil {
+		return err
+	}
+	if auth != id.AuthToken {
+		return fmt.Errorf("GetAuthToken returned unexpected value: %s, expected: %s", auth, id.AuthToken)
+	}
+
+	priv, err := dm.GetPrivateKey(id.Uid)
+	if err != nil {
+		return err
+	}
+	if !bytes.Equal(priv, id.PrivateKey) {
+		return fmt.Errorf("GetPrivateKey returned unexpected value: %s, expected: %s", priv, id.PrivateKey)
+	}
+
+	pub, err := dm.GetPublicKey(id.Uid)
+	if err != nil {
+		return err
+	}
+	if !bytes.Equal(pub, id.PublicKey) {
+		return fmt.Errorf("GetPublicKey returned unexpected value: %s, expected: %s", pub, id.PublicKey)
+	}
+
+	uid, err := dm.GetUuidForPublicKey(id.PublicKey)
+	if err != nil {
+		return err
+	}
+	if !bytes.Equal(uid[:], id.Uid[:]) {
+		return fmt.Errorf("GetUuidForPublicKey returned unexpected value: %s, expected: %s", uid, id.Uid)
+	}
+
+	return nil
 }
