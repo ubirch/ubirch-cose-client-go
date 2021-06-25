d2661a5bfe8a45384a481256d96c5cf526f1aa10
Roxana Meixner <roxana.meixner@ubirch.com>
2021-06-25 16:03:03 +0200

    skid handler tests (96.2%)

--- main/skid_handler_test.go
+++ main/skid_handler_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"sync"
 	"testing"
 	"time"
 
@@ -23,6 +25,10 @@ func TestNewSkidHandler(t *testing.T) {
 
 	if s.maxCertLoadFailCount != 3 {
 		t.Errorf("wrong interval set: %d, expected: 3", s.maxCertLoadFailCount)
+	}
+
+	if s.certLoadFailCounter != 0 {
+		t.Errorf("s.certLoadFailCounter != 0: is %d", s.certLoadFailCounter)
 	}
 
 	if s.certLoadInterval != time.Hour {
@@ -50,6 +56,82 @@ func TestNewSkidHandler(t *testing.T) {
 	testUUIDs = []uuid.UUID{}
 }
 
+func TestNewSkidHandler_ReloadEveryMinute(t *testing.T) {
+	c := &ubirch.ECDSACryptoContext{}
+
+	s := NewSkidHandler(mockGetCertificateList, mockGetUuidFindsNothing, c.EncodePublicKey, true)
+
+	if len(s.skidStore) != 0 {
+		t.Errorf("SKIDs were loaded with mockGetUuidFindsNothing")
+	}
+
+	if s.maxCertLoadFailCount != 60 {
+		t.Errorf("wrong interval set: %d, expected: 60", s.maxCertLoadFailCount)
+	}
+
+	if s.certLoadFailCounter != 0 {
+		t.Errorf("s.certLoadFailCounter != 0: is %d", s.certLoadFailCounter)
+	}
+
+	if s.certLoadInterval != time.Minute {
+		t.Errorf("wrong interval set: %s, expected: %s", s.certLoadInterval, time.Minute)
+	}
+}
+
+func TestNewSkidHandler_BadGetCertificateList(t *testing.T) {
+	s := &SkidHandler{
+		skidStore:      map[uuid.UUID][]byte{},
+		skidStoreMutex: &sync.RWMutex{},
+
+		certLoadFailCounter:  0,
+		maxCertLoadFailCount: 3,
+
+		getCerts: mockBadGetCertificateList,
+	}
+
+	s.loadSKIDs()
+
+	if len(s.skidStore) != 0 {
+		t.Errorf("SKIDs were loaded with mockBadGetCertificateList")
+	}
+
+	if s.certLoadFailCounter != 1 {
+		t.Errorf("unexpected s.certLoadFailCounter value after 1 fail : %d", s.certLoadFailCounter)
+	}
+}
+
+func TestNewSkidHandler_BadGetCertificateList_MaxCertLoadFailCount(t *testing.T) {
+	s := &SkidHandler{
+		skidStore:      map[uuid.UUID][]byte{},
+		skidStoreMutex: &sync.RWMutex{},
+
+		certLoadFailCounter:  0,
+		maxCertLoadFailCount: 3,
+
+		getCerts: mockBadGetCertificateList,
+	}
+
+	s.skidStore[uuid.New()] = make([]byte, 8)
+	s.skidStore[uuid.New()] = make([]byte, 8)
+	s.skidStore[uuid.New()] = make([]byte, 8)
+
+	for i := 1; i <= s.maxCertLoadFailCount; i++ {
+		if len(s.skidStore) != 3 {
+			t.Errorf("SKIDs were cleared before maxCertLoadFailCount")
+		}
+
+		s.loadSKIDs()
+
+		if s.certLoadFailCounter != i {
+			t.Errorf("unexpected s.certLoadFailCounter value after %d fail : %d", i, s.certLoadFailCounter)
+		}
+	}
+
+	if len(s.skidStore) != 0 {
+		t.Errorf("SKIDs were not cleared after maxCertLoadFailCount")
+	}
+}
+
 func mockGetCertificateList() ([]Certificate, error) {
 	filename := "test-cert-list.json"
 	fileHandle, err := os.Open(filename)
@@ -67,8 +149,16 @@ func mockGetCertificateList() ([]Certificate, error) {
 	return certs, nil
 }
 
+func mockBadGetCertificateList() ([]Certificate, error) {
+	return nil, fmt.Errorf("test error")
+}
+
 func mockGetUuid(pubKey []byte) (uuid.UUID, error) {
 	newUUID := uuid.New()
 	testUUIDs = append(testUUIDs, newUUID)
 	return newUUID, nil
+}
+
+func mockGetUuidFindsNothing(pubKey []byte) (uuid.UUID, error) {
+	return uuid.Nil, ErrNotExist
 }
