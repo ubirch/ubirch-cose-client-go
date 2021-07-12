c77b5d0da4e054e8579e7daa6b89883261135ceb
Roxana Meixner <roxana.meixner@ubirch.com>
2021-07-12 23:46:44 +0200

    added doc

--- main/key_derivator.go
+++ main/key_derivator.go
@@ -9,16 +9,16 @@ type KeyDerivator interface {
 }
 
 type Argon2idKeyDerivator struct {
-	time    uint32
-	memory  uint32
-	threads uint8
-	keyLen  uint32
+	time    uint32 // the time parameter specifies the number of passes over the memory
+	memory  uint32 // the memory parameter specifies the size of the memory in KiB
+	threads uint8  // the threads parameter specifies the number of threads and can be adjusted to the numbers of available CPUs
+	keyLen  uint32 // the length resulting derived key in byte
 }
 
 func NewDefaultArgon2idKeyDerivator() *Argon2idKeyDerivator {
 	return &Argon2idKeyDerivator{
-		time:    1, // https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-argon2-03#section-9.3
-		memory:  64 * 1024,
+		time:    1,         // https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-argon2-03#section-9.3
+		memory:  64 * 1024, // 64 MB
 		threads: 4,
 		keyLen:  24,
 	}
