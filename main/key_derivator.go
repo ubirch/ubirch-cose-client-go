bdb786be7757a0336e6e6dcde987bee388429be7
Roxana Meixner <roxana.meixner@ubirch.com>
2021-07-13 11:38:01 +0200

    ScryptKeyDerivator

--- main/key_derivator.go
+++ main/key_derivator.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"golang.org/x/crypto/argon2"
+	"golang.org/x/crypto/scrypt"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type KeyDerivator interface {
@@ -29,4 +32,29 @@ func NewDefaultArgon2idKeyDerivator() *Argon2idKeyDerivator {
 // https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-argon2-03
 func (kd *Argon2idKeyDerivator) GetDerivedKey(password, salt []byte) []byte {
 	return argon2.IDKey(password, salt, kd.time, kd.memory, kd.threads, kd.keyLen)
+}
+
+type ScryptKeyDerivator struct {
+	N, r, p, keyLen int
+}
+
+func NewDefaultScryptKeyDerivator() *ScryptKeyDerivator {
+	// The recommended parameters for interactive logins as of 2017 are
+	// N=32768, r=8 and p=1.
+	return &ScryptKeyDerivator{
+		N:      32 * 1024,
+		r:      8,
+		p:      1,
+		keyLen: 24,
+	}
+}
+
+// GetDerivedKey derives a key from the password, salt, and cost parameters using Scrypt
+// returning the derived key of length kd.keyLen
+func (kd *ScryptKeyDerivator) GetDerivedKey(password, salt []byte) []byte {
+	dk, err := scrypt.Key(password, salt, kd.N, kd.r, kd.p, kd.keyLen)
+	if err != nil {
+		log.Errorf("scrypt key derivation error: %v", err)
+	}
+	return dk
 }
