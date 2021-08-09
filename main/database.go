6565935d2dea4b3272613f1c15bdc3ddfc9a73f0
Roxana Meixner <roxana.meixner@ubirch.com>
2021-08-09 12:47:53 +0200

    remove "test" suffix from database table name

--- main/database.go
+++ main/database.go
@@ -27,7 +27,7 @@ import (
 
 const (
 	PostgreSql                  string = "postgres"
-	PostgreSqlIdentityTableName string = "cose_identity_hsm_test" // FIXME
+	PostgreSqlIdentityTableName string = "cose_identity_hsm"
 )
 
 const (
