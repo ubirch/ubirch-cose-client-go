1495bf3682bc59c7d1216f28246f305f89728700
Filip Thamasett <filip.softcrafts@gmail.com>
2021-08-31 11:37:51 +0200

    adding testcases for fetcher and identity registerer

---
+++ main/http-server/CSR_fetcher_test.go
@@ -0,0 +1,102 @@
+package http_server
+
+import (
+	"errors"
+	"github.com/go-chi/chi"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"path"
+	"testing"
+)
+
+const testAuth = "auth"
+
+func TestFetchCSR(t *testing.T) {
+
+	testUuid := uuid.New()
+	testCases := []struct {
+		name      string
+		callerUrl string
+		auth      string
+		getCsR    GetCSR
+		tcChecks  func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:      "happy path",
+			callerUrl: path.Join("/", testUuid.String(), CSREndpoint),
+			auth:      testAuth,
+			getCsR: func(uid uuid.UUID) (csr []byte, err error) {
+				return uid.NodeID(), nil
+			},
+			tcChecks: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, testUuid.NodeID(), recorder.Body.Bytes())
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		{
+			name:      "unauthorized",
+			callerUrl: path.Join("/", uuid.NewString(), CSREndpoint),
+			auth:      "",
+			getCsR: func(uid uuid.UUID) (csr []byte, err error) {
+				return uid.NodeID(), nil
+			},
+			tcChecks: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Contains(t, recorder.Body.String(), http.StatusText(http.StatusUnauthorized))
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
+		{
+			name:      "invalid uuid",
+			callerUrl: path.Join("/", "2222", CSREndpoint),
+			auth:      testAuth,
+			getCsR: func(uid uuid.UUID) (csr []byte, err error) {
+				return uid.NodeID(), nil
+			},
+			tcChecks: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Contains(t, recorder.Body.String(), "invalid UUID")
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name:      "unknown uuid",
+			callerUrl: path.Join("/", testUuid.String(), CSREndpoint),
+			auth:      testAuth,
+			getCsR: func(uid uuid.UUID) (csr []byte, err error) {
+				return nil, ErrUnknown
+			},
+			tcChecks: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Contains(t, recorder.Body.String(), "unknown identity")
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name:      "unknown uuid internal server error",
+			callerUrl: path.Join("/", testUuid.String(), CSREndpoint),
+			auth:      testAuth,
+			getCsR: func(uid uuid.UUID) (csr []byte, err error) {
+				return nil, errors.New("unknown error")
+			},
+			tcChecks: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.NotEmpty(t, recorder.Body.String())
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			router := chi.NewRouter()
+
+			recorder := httptest.NewRecorder()
+			url := path.Join(UUIDPath, CSREndpoint)
+			router.Get(url, FetchCSR(testAuth, c.getCsR))
+
+			req := httptest.NewRequest(http.MethodGet, c.callerUrl, nil)
+			req.Header.Add(AuthHeader, c.auth)
+			router.ServeHTTP(recorder, req)
+			c.tcChecks(t, recorder)
+		})
+	}
+
+}
---
+++ main/http-server/identity_registerer_test.go
@@ -0,0 +1,137 @@
+package http_server
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/go-chi/chi"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRegister(t *testing.T) {
+
+	byteStr := []byte("csr")
+
+	testCases := []struct {
+		name        string
+		auth        string
+		contentType string
+		body        RegistrationPayload
+		initId      InitializeIdentity
+		tcChecks    func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:        "happy path",
+			auth:        testAuth,
+			contentType: JSONType,
+			body: RegistrationPayload{
+				Uid: uuid.New(),
+			},
+			initId: func(uid uuid.UUID) (csr []byte, err error) {
+				return byteStr, nil
+			},
+			tcChecks: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Contains(t, string(byteStr), recorder.Body.String())
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		{
+			name:        "wrong content type header",
+			auth:        testAuth,
+			contentType: BinType,
+			body: RegistrationPayload{
+				Uid: uuid.New(),
+			},
+			initId: func(uid uuid.UUID) (csr []byte, err error) {
+				return byteStr, nil
+			},
+			tcChecks: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Contains(t, recorder.Body.String(), "invalid content-type")
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name:        "no uuid",
+			auth:        testAuth,
+			contentType: JSONType,
+			body: RegistrationPayload{
+				Uid: uuid.Nil,
+			},
+			initId: func(uid uuid.UUID) (csr []byte, err error) {
+				return byteStr, nil
+			},
+			tcChecks: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Contains(t, recorder.Body.String(), "empty uuid")
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name:        "no password",
+			auth:        testAuth,
+			contentType: JSONType,
+			body: RegistrationPayload{
+				Uid: uuid.New(),
+				Pwd: "not supported anymore",
+			},
+			initId: func(uid uuid.UUID) (csr []byte, err error) {
+				return byteStr, nil
+			},
+			tcChecks: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Contains(t, recorder.Body.String(), "setting password is not longer supported.")
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name:        "not authorized",
+			auth:        "",
+			contentType: JSONType,
+			body: RegistrationPayload{
+				Uid: uuid.New(),
+			},
+			initId: func(uid uuid.UUID) (csr []byte, err error) {
+				return byteStr, nil
+			},
+			tcChecks: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Contains(t, recorder.Body.String(), http.StatusText(http.StatusUnauthorized))
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
+		{
+			name:        "conflict",
+			auth:        testAuth,
+			contentType: JSONType,
+			body: RegistrationPayload{
+				Uid: uuid.New(),
+			},
+			initId: func(uid uuid.UUID) (csr []byte, err error) {
+				return nil, ErrAlreadyInitialized
+			},
+			tcChecks: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Contains(t, recorder.Body.String(), ErrAlreadyInitialized.Error())
+				require.Equal(t, http.StatusConflict, recorder.Code)
+			},
+		},
+	}
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			router := chi.NewRouter()
+
+			recorder := httptest.NewRecorder()
+			router.Put(RegisterEndpoint, Register(testAuth, c.initId))
+
+			payloadBuf := new(bytes.Buffer)
+			err := json.NewEncoder(payloadBuf).Encode(c.body)
+			require.NoError(t, err)
+
+			req := httptest.NewRequest(http.MethodPut, RegisterEndpoint, payloadBuf)
+			req.Header.Add(AuthHeader, c.auth)
+			req.Header.Add("Content-Type", c.contentType)
+
+			router.ServeHTTP(recorder, req)
+			c.tcChecks(t, recorder)
+		})
+	}
+}
