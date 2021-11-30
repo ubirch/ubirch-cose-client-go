0c3198177a73793a013da0692a7bcf78f3bf168f
Roxana Meixner <roxana.meixner@ubirch.com>
2021-11-30 11:56:13 +0100

    * add resp_test.go and test SendResponse function * fix health and readiness check tests failing due to race condition and move to resp_test.go

--- main/http-server/http_server_test.go
+++ main/http-server/http_server_test.go
@@ -2,8 +2,6 @@ package http_server
 
 import (
 	"context"
-	"fmt"
-	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -35,80 +33,6 @@ func TestServeError(t *testing.T) {
 			defer cancel()
 			err := c.httpServer.Serve(ctx)
 			c.tcChecks(t, err)
-		})
-	}
-}
-
-func TestServe_Health_Ready(t *testing.T) {
-
-	testCases := []struct {
-		name      string
-		reqUrl    string
-		readyFunc func() error
-		tcChecks  func(t *testing.T, err error, resp *http.Response)
-	}{
-		{
-			name:   "happy path readyz",
-			reqUrl: "http://localhost:1234/readyz",
-			readyFunc: func() error {
-				return nil
-			},
-			tcChecks: func(t *testing.T, err error, resp *http.Response) {
-				require.NoError(t, err)
-				require.Equal(t, http.StatusOK, resp.StatusCode)
-			},
-		},
-		{
-			name:   "happy path heathz",
-			reqUrl: "http://localhost:1234/healthz",
-			readyFunc: func() error {
-				return nil
-			},
-			tcChecks: func(t *testing.T, err error, resp *http.Response) {
-				require.NoError(t, err)
-				require.Equal(t, http.StatusOK, resp.StatusCode)
-			},
-		},
-		{
-			name:   "error readiness func",
-			reqUrl: "http://localhost:1234/readyz",
-			readyFunc: func() error {
-				return fmt.Errorf("something")
-			},
-			tcChecks: func(t *testing.T, err error, resp *http.Response) {
-				require.NoError(t, err)
-				require.Equal(t, http.StatusServiceUnavailable, resp.StatusCode)
-			},
-		},
-	}
-	for _, c := range testCases {
-		t.Run(c.name, func(t *testing.T) {
-			var readinessChecks []func() error
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-
-			httpServer := HTTPServer{
-				Router: NewRouter(),
-				Addr:   "localhost:1234",
-				TLS:    false,
-			}
-
-			readinessChecks = append(readinessChecks, c.readyFunc)
-
-			httpServer.Router.Get("/healthz", Healthz("serverID"))
-			httpServer.Router.Get("/readyz", Readyz("serverID", readinessChecks))
-
-			go func() {
-				err := httpServer.Serve(ctx)
-				require.NoError(t, err)
-			}()
-
-			client := http.Client{}
-			reqReady, err := http.NewRequest(http.MethodGet, c.reqUrl, nil)
-			require.NoError(t, err)
-			respReady, err := client.Do(reqReady)
-
-			c.tcChecks(t, err, respReady)
 		})
 	}
 }
---
+++ main/http-server/resp_test.go
@@ -0,0 +1,85 @@
+package http_server
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSendResponse(t *testing.T) {
+	header := http.Header{}
+	header.Set("Content-Type", TextType)
+	header.Set(AuthHeader, testAuth)
+
+	resp := HTTPResponse{
+		StatusCode: http.StatusTeapot,
+		Header:     header,
+		Content:    []byte("hello world"),
+	}
+
+	w := httptest.NewRecorder()
+
+	SendResponse(w, resp)
+
+	assert.Equal(t, resp.StatusCode, w.Code)
+	assert.Equal(t, TextType, w.Header().Get("Content-Type"))
+	assert.Equal(t, testAuth, w.Header().Get(AuthHeader))
+	assert.Equal(t, resp.Content, w.Body.Bytes())
+}
+
+func TestHealth(t *testing.T) {
+	const server = "test server"
+
+	w := httptest.NewRecorder()
+
+	Healthz(server)(w, nil)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, server, w.Header().Get("Server"))
+	assert.Equal(t, TextType, w.Header().Get("Content-Type"))
+	assert.Equal(t, []byte(http.StatusText(http.StatusOK)), w.Body.Bytes())
+}
+
+func TestReady(t *testing.T) {
+
+	testCases := []struct {
+		name      string
+		readyFunc func() error
+		code      int
+	}{
+		{
+			name: "happy path readyz",
+			readyFunc: func() error {
+				return nil
+			},
+			code: http.StatusOK,
+		},
+		{
+			name: "error readiness func",
+			readyFunc: func() error {
+				return fmt.Errorf("something")
+			},
+			code: http.StatusServiceUnavailable,
+		},
+	}
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			const server = "test server"
+			var readinessChecks []func() error
+
+			readinessChecks = append(readinessChecks, c.readyFunc)
+
+			w := httptest.NewRecorder()
+
+			Readyz(server, readinessChecks)(w, nil)
+
+			assert.Equal(t, c.code, w.Code)
+			assert.Equal(t, server, w.Header().Get("Server"))
+			assert.Equal(t, TextType, w.Header().Get("Content-Type"))
+			assert.Equal(t, []byte(http.StatusText(c.code)), w.Body.Bytes())
+		})
+	}
+}
