79f305f210df6c15bb6fadbf2b3b856fcfc00f79
Roxana Meixner <roxana.meixner@ubirch.com>
2021-12-03 20:14:59 +0100

    test version blocks until network available

--- main/main.go
+++ main/main.go
@@ -76,6 +76,21 @@ func main() {
 		defer profiling.StopBlockProfileRecording(file)
 	}
 
+	storageManager, err := GetStorageManager(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer storageManager.Close()
+	readinessChecks = append(readinessChecks, storageManager.IsReady)
+
+	err = storageManager.IsReady()
+	for err != nil {
+		log.Infof("db not ready yet")
+		time.Sleep(200 * time.Millisecond)
+		err = storageManager.IsReady()
+	}
+	log.Info("db ready")
+
 	// initialize COSE service
 	cryptoCtx, err := ubirch.NewECDSAPKCS11CryptoContext(conf.PKCS11Module, conf.PKCS11ModulePin,
 		conf.PKCS11ModuleSlotNr, true, 1, 50*time.Millisecond)
@@ -97,13 +112,6 @@ func main() {
 		// a session on every incoming signing request.
 		log.Warnf("unable to set up session with HSM: %v", err)
 	}
-
-	storageManager, err := GetStorageManager(conf)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer storageManager.Close()
-	readinessChecks = append(readinessChecks, storageManager.IsReady)
 
 	protocol := NewProtocol(storageManager, conf)
 
