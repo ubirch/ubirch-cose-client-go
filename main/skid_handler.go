c74c87a783d891d0fe564325d1f4fa9058870b5d
Roxana Meixner <roxana.meixner@ubirch.com>
2022-01-27 13:31:09 +0100

    refactoring: extract methods for enhanced readability and maintainability

--- main/skid_handler.go
+++ main/skid_handler.go
@@ -57,8 +57,6 @@ func NewSkidHandler(certs GetCertificateList, uid GetUuid, enc EncodePublicKey, 
 		skidStore:      nil,
 		skidStoreMutex: &sync.RWMutex{},
 
-		certLoadFailCounter: 0,
-
 		getCerts:  certs,
 		getUuid:   uid,
 		encPubKey: enc,
@@ -91,33 +89,7 @@ func (s *SkidHandler) loadSKIDs() {
 	certs, err := s.getCerts()
 	if err != nil {
 		log.Errorf("unable to retrieve public key certificate list (trustList): %v", err)
-
-		s.certLoadFailCounter++
-		s.isCertServerAvailable.Store(false)
-
-		if !s.lastSuccessfulCertLoad.IsZero() {
-			errMsg := fmt.Sprintf("trustList could not be loaded since %s", s.lastSuccessfulCertLoad.Format(timeLayout))
-
-			if s.certLoadFailCounter < s.maxCertLoadFailCount {
-				// we have not yet reached the maximum amount of failed attempts to load the certificate list,
-				// return and try again later
-				log.Warnf(errMsg+", local SKID lookup will be cleared in %s if this issue persists",
-					time.Duration(s.maxCertLoadFailCount-s.certLoadFailCounter)*s.certLoadInterval)
-			} else if s.certLoadFailCounter == s.maxCertLoadFailCount {
-				// we have reached the maximum amount of failed attempts to load the certificate list,
-				// clear the SKID lookup
-				log.Errorf(errMsg+", clearing local SKID lookup after %d failed attempts to load public key certificate list",
-					s.certLoadFailCounter)
-
-				s.resetSkidStore()
-			} else {
-				// we surpassed the maximum amount of failed attempts to load the certificate list,
-				// SKID lookup has been cleared
-				log.Warnf(errMsg+", local SKID lookup was cleared %s ago",
-					time.Duration(s.certLoadFailCounter-s.maxCertLoadFailCount)*s.certLoadInterval)
-			}
-		}
-
+		s.handleCertLoadFail()
 		return
 	}
 
@@ -130,73 +102,112 @@ func (s *SkidHandler) loadSKIDs() {
 
 	// go through certificate list and match known public keys
 	for _, cert := range certs {
-		skidString := base64.StdEncoding.EncodeToString(cert.Kid)
-
-		if len(cert.Kid) != SkidLen {
-			log.Errorf("%s: invalid KID length: %d, expected: %d", skidString, len(cert.Kid), SkidLen)
+		uid, matchedSkid := s.matchCertificate(cert)
+		if matchedSkid == nil {
 			continue
 		}
 
-		// get public key from certificate
-		certificate, err := x509.ParseCertificate(cert.RawData)
-		if err != nil {
-			log.Errorf("%s: parsing x509 certificate failed: %v", skidString, err)
-			continue
-		}
-
-		pubKeyPEM, err := s.encPubKey(certificate.PublicKey)
-		if err != nil {
-			log.Debugf("%s: unable to encode public key from x509 certificate: %v", skidString, err)
-			continue
-		}
-
-		// look up matching UUID for public key
-		uid, err := s.getUuid(pubKeyPEM)
-		if err != nil {
-			if err == ErrNotExist {
-				log.Debugf("%s: public key from x509 certificate does not match any known identity", skidString)
-			} else {
-				log.Errorf("%s: public key lookup failed: %v", skidString, err)
-			}
-			continue
-		}
-		log.Debugf("%s: public key match: %s", skidString, uid)
-
-		matchedSkid := skidCtx{
-			SKID:      cert.Kid,
-			NotBefore: certificate.NotBefore,
-			NotAfter:  certificate.NotAfter,
-		}
-
-		// check validity of certificate
-		now := time.Now()
-
-		if now.After(matchedSkid.NotAfter) {
-			log.Debugf("%s: certificate expired: valid until %s", skidString, matchedSkid.NotAfter.Format(timeLayout))
-			matchedSkid.expired = true
-		} else if now.Before(matchedSkid.NotBefore) {
-			log.Debugf("%s: certificate not yet valid: valid from %s", skidString, matchedSkid.NotBefore.Format(timeLayout))
-		} else {
-			matchedSkid.Valid = true
-		}
-
-		if previouslyMatchedSkid, ok := tempSkidStore[uid]; ok {
-			if previouslyMatchedSkid.Valid && !matchedSkid.Valid {
-				continue
-			}
-
-			// if there is more than one valid certificate, use the newer one, i.e. the one that starts being valid at a later time
-			if (previouslyMatchedSkid.Valid && matchedSkid.Valid) || (!previouslyMatchedSkid.Valid && !matchedSkid.Valid) {
-				if matchedSkid.NotBefore.Before(previouslyMatchedSkid.NotBefore) {
-					continue
-				}
-			}
-		}
-
-		tempSkidStore[uid] = matchedSkid
+		s.checkCertificateValidity(uid, matchedSkid, &tempSkidStore)
 	}
 
 	s.updateSkidStore(tempSkidStore)
+}
+
+func (s *SkidHandler) handleCertLoadFail() {
+	s.certLoadFailCounter++
+	s.isCertServerAvailable.Store(false)
+
+	if !s.lastSuccessfulCertLoad.IsZero() {
+		errMsg := fmt.Sprintf("trustList could not be loaded since %s", s.lastSuccessfulCertLoad.Format(timeLayout))
+
+		if s.certLoadFailCounter < s.maxCertLoadFailCount {
+			// we have not yet reached the maximum amount of failed attempts to load the certificate list,
+			// return and try again later
+			log.Warnf(errMsg+", local SKID lookup will be cleared in %s if this issue persists",
+				time.Duration(s.maxCertLoadFailCount-s.certLoadFailCounter)*s.certLoadInterval)
+		} else if s.certLoadFailCounter == s.maxCertLoadFailCount {
+			// we have reached the maximum amount of failed attempts to load the certificate list,
+			// clear the SKID lookup
+			log.Errorf(errMsg+", clearing local SKID lookup after %d failed attempts to load public key certificate list",
+				s.certLoadFailCounter)
+
+			s.resetSkidStore()
+		} else {
+			// we surpassed the maximum amount of failed attempts to load the certificate list,
+			// SKID lookup has been cleared
+			log.Warnf(errMsg+", local SKID lookup was cleared %s ago",
+				time.Duration(s.certLoadFailCounter-s.maxCertLoadFailCount)*s.certLoadInterval)
+		}
+	}
+}
+
+func (s *SkidHandler) matchCertificate(cert Certificate) (uuid.UUID, *skidCtx) {
+	skidString := base64.StdEncoding.EncodeToString(cert.Kid)
+
+	if len(cert.Kid) != SkidLen {
+		log.Errorf("%s: invalid KID length: %d, expected: %d", skidString, len(cert.Kid), SkidLen)
+		return uuid.Nil, nil
+	}
+
+	// get public key from certificate
+	certificate, err := x509.ParseCertificate(cert.RawData)
+	if err != nil {
+		log.Errorf("%s: parsing x509 certificate failed: %v", skidString, err)
+		return uuid.Nil, nil
+	}
+
+	pubKeyPEM, err := s.encPubKey(certificate.PublicKey)
+	if err != nil {
+		log.Debugf("%s: unable to encode public key from x509 certificate: %v", skidString, err)
+		return uuid.Nil, nil
+	}
+
+	// look up matching UUID for public key
+	uid, err := s.getUuid(pubKeyPEM)
+	if err != nil {
+		if err == ErrNotExist {
+			log.Debugf("%s: public key from x509 certificate does not match any known identity", skidString)
+		} else {
+			log.Errorf("%s: public key lookup failed: %v", skidString, err)
+		}
+		return uuid.Nil, nil
+	}
+	log.Debugf("%s: public key match: %s", skidString, uid)
+
+	return uid, &skidCtx{
+		SKID:      cert.Kid,
+		NotBefore: certificate.NotBefore,
+		NotAfter:  certificate.NotAfter,
+	}
+}
+
+func (s *SkidHandler) checkCertificateValidity(uid uuid.UUID, skid *skidCtx, skidStore *map[uuid.UUID]skidCtx) {
+	skidString := base64.StdEncoding.EncodeToString(skid.SKID)
+	now := time.Now()
+
+	if now.After(skid.NotAfter) {
+		log.Debugf("%s: certificate expired: valid until %s", skidString, skid.NotAfter.Format(timeLayout))
+		skid.expired = true
+	} else if now.Before(skid.NotBefore) {
+		log.Debugf("%s: certificate not yet valid: valid from %s", skidString, skid.NotBefore.Format(timeLayout))
+	} else {
+		skid.Valid = true
+	}
+
+	if previouslyMatchedSkid, ok := (*skidStore)[uid]; ok {
+		if previouslyMatchedSkid.Valid && !skid.Valid {
+			return
+		}
+
+		// if the certificates are both valid or invalid, use the newer one, i.e. the one that starts being valid at a later time
+		if (previouslyMatchedSkid.Valid && skid.Valid) || (!previouslyMatchedSkid.Valid && !skid.Valid) {
+			if skid.NotBefore.Before(previouslyMatchedSkid.NotBefore) {
+				return
+			}
+		}
+	}
+
+	(*skidStore)[uid] = *skid
 }
 
 func (s *SkidHandler) setSkidStore(newSkidStore map[uuid.UUID]skidCtx) {
